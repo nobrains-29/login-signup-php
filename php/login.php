15c22a2b472e9f521912e46e54b083d56ba23dab
Archit Tripathi <nobrains110@gmail.com>
2023-05-11 14:58:57 +0530

    Initial commit

---
+++ php/login.php
@@ -0,0 +1,33 @@
+<?php
+if (isset($_POST['login'])) {
+	header('Access-Control-Allow-Origin: *');
+	header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+	header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+	$user = $_POST['username'];
+	$pass = $_POST['password'];
+	$servername = "localhost";
+	$username = "root";
+	$password = "root123";
+	$dbname = "guvi";
+
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	$sql = "SELECT * FROM auth WHERE useremail='$user' AND userpassword='$pass'";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows == 1) {
+		die(json_encode(array('status' => true)));
+	} else {
+		die(json_encode(array("status" => false)));
+	}
+
+
+	$conn->close();
+	$text = $user;
+	echo json_encode(array('text' => $text, 'name' => "Archit" /* and anything else you want */));
+
+}
+
+?>
